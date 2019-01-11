7f95f706b2c8c49e795d3734044e421ecedb9c42
Veetaha <gerzoh1@gmail.com>
2019-01-11 04:26:48 +0200

    added README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# Webprogbase webite 'Veetaha'
+This is my university web project.
+You can check it out [**here**](https://veetaha.herokuapp.com) (you may need to wait for a little while, as the server might be idling and needs to restart once you follow the link).
+
+
+It is built via `MEAN` stack (`MongoDB, ExpressJS, Angular 7, NodeJS`) with the use of `TypeScript` and `GraphQL`. Feel free to contribute =).
--- telegram-bot/app.ts
+++ telegram-bot/app.ts
@@ -1,3 +1,5 @@
+// TODO Ressurect Telegram Bot
+
 import * as TelegramBot from 'node-telegram-bot-api';
 import * as Config from "./config";
 import * as GqlReq from 'graphql-request';
@@ -51,42 +53,3 @@ bot.onText(/\/start/, async msg => {
         console.error(err);
     }
 });
-
-// bot.onText(/wallets/, async function (msg) {
-//     const fromId = msg.from.id;
-//     const name = msg.from.username;
-//     try {
-//         const response = await fetch(`https://primat24.herokuapp.com/api/telegram/users/wallets?userName=${name}`);
-//         if (!response) {
-//             return bot.sendMessage(fromId, `Server not work`);
-//         }
-//         const data = await response.json();
-//         if (data.err) {
-//             return bot.sendMessage(fromId, `You are not registered`);
-
-//         }
-//         console.log(`username ${name} id ${fromId}`);
-//         let str: string = `Your wallets \n`;
-//         data.forEach(element => {
-//             str = `${str}${element.currency}:${element.sum}\n`;
-//         });
-//         bot.sendMessage(fromId, str);
-//     } catch (err) {
-//         return bot.sendMessage(fromId, `Server not work`);
-
-//     }
-// });
-// bot.onText(/info/, async function (msg) {
-//     const fromId = msg.from.id;
-//     const name = msg.from.username;
-//     console.log()
-//     const str = "/wallets - your wallets\n/info - info about bot";
-//     bot.sendMessage(fromId, str);
-// });
-// // bot.on("text", async function (msg) {
-// //     const fromId = msg.from.id;
-// //     const name = msg.from.username;
-// //     console.log(`username ${name} id ${fromId}`);
-// //     const str = "your text";
-// //     bot.sendMessage(fromId, str);
-// // });
